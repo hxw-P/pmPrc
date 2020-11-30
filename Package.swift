9ee6d1941cc566e3eb8f981a204f6929b779a121
hxw <1375166613@qq.com>
2020-11-30 15:30:41 +0800

    【华晓伟】：修改oc测试方法

--- Package.swift
+++ Package.swift
@@ -15,7 +15,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ocFunc",
-            dependencies: []),
+            dependencies: [],
+            sources: ["testOC.h", "testOC.m", "testOC1.h", "testOC1.m"],
+            publicHeadersPath: "include"),
         .target(
             name: "swiftFunc",
             dependencies: []),
