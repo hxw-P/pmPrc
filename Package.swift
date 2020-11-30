6e942e6cbf5ea11a8e3c650b5928aaadf2ea771c
hxw <1375166613@qq.com>
2020-11-30 15:57:53 +0800

    测试

--- Package.swift
+++ Package.swift
@@ -15,10 +15,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ocFunc",
-            dependencies: [],
-            path: ".",
-            sources: ["testOC.h", "testOC.m", "testOC1.h", "testOC1.m"],
-            publicHeadersPath: "include"),
+            dependencies: []),
         .target(
             name: "swiftFunc",
             dependencies: []),
