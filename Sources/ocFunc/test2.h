64cc63f2321f1b7b6efece1a07e00f595714b89c
hxw <1375166613@qq.com>
2020-11-30 14:45:56 +0800

    【华晓伟】：添加oc方法测试

---
+++ Sources/ocFunc/test2.h
@@ -0,0 +1,18 @@
+//
+//  test2.h
+//  
+//
+//  Created by 华晓伟 on 2020/11/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface test2 : NSObject
+
++ (NSString *)pr;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sources/ocFunc/test2.m
@@ -0,0 +1,16 @@
+//
+//  test2.m
+//  
+//
+//  Created by 华晓伟 on 2020/11/30.
+//
+
+#import "test2.h"
+
+@implementation test2
+
++ (NSString *)pr {
+    return @"test2";
+}
+
+@end
