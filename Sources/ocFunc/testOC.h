69b9c5f16872ee25666f65038d3bb0d4f0b49099
hxw <1375166613@qq.com>
2020-12-01 11:04:54 +0800

    测试

---
+++ Sources/ocFunc/testOC.h
@@ -0,0 +1,18 @@
+//
+//  testOC.h
+//  
+//
+//  Created by 华晓伟 on 2020/11/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface testOC : NSObject
+
++ (NSString *)pr;
+
+@end
+
+NS_ASSUME_NONNULL_END
